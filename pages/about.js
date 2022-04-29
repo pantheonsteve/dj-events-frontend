1d7e55bedd641c4e4276e0d89b859fb4b48237b8
Steve Bresnick <steve.bresnick@pantheon.io>
2022-04-29 11:47:33 -0700

    commit #2

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,13 @@
-export default function about() {
+import Link from "next/link"
+import Layout from '../components/Layout'
+
+export default function AboutPage() {
   return (
-    <div>
+    <Layout title='About DJ Events'>
         <h1>About</h1>
         <p>This is an app to find the latest information about concerts and other music events</p>
-    </div>
+        <p>Version: 1.0.0</p>
+        <Link href='/'>Home</Link>
+    </Layout>
   )
 }
