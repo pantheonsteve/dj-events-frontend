2bb6138acb8178d40a3c1cdb3db3e5fae39fe75e
Steve Bresnick <steve.bresnick@pantheon.io>
2022-04-29 07:17:49 -0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function about() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>This is an app to find the latest information about concerts and other music events</p>
+    </div>
+  )
+}
